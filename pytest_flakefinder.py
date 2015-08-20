281af645094a9dfbdbccb66d5972b86a30733164
Sergei Tsarev <sergei@dropbox.com>
2015-08-19 18:22:19 -0700

    Extend the flake finder plugin to take a --flake-max-minutes parameter
    Summary:
    We need to have an upper bound on the amount of time flake finder runs in a CI.
    Once we reach our runtime limit, we skip the rest of the testplan.
    
    Test Plan: - manual testing

--- pytest_flakefinder.py
+++ pytest_flakefinder.py
@@ -1,5 +1,11 @@
 """Run a test multiple times to see if it's flaky."""
+
+from __future__ import absolute_import
+
 import pytest
+
+import time
+
 
 def pytest_addoption(parser):
     """Register for arguments."""
@@ -8,6 +14,8 @@ def pytest_addoption(parser):
                     action='store_true', default=False,
                     help="Enable flake finder.  This will create multiple copies of all the selected tests.")
     group.addoption("--no-flake-finder", dest="flake_finder_enable", action='store_false')
+    group.addoption("--flake-max-minutes", dest="flake_max_minutes", default=0, type=int, action="store",
+                    help="Don't run for longer than this parameter. Default is 0, which is no limit")
     group.addoption("--flake-runs", metavar="flake_runs",
                     action='store', dest="flake_runs", type=int, default=50)
 
@@ -25,8 +33,17 @@ class FlakeFinderPlugin(object):
 
     def __init__(self, config):
         self.flake_runs = config.getoption('flake_runs')
+        self.expires = config.getoption('flake_max_minutes')
+        if self.expires:
+            self.expires = time.time() + self.expires * 60
 
     @pytest.mark.trylast
     def pytest_collection_modifyitems(self, items):
         """Multiply those tests!"""
         items[:] = items * self.flake_runs
+
+    def pytest_runtest_call(self, item):
+        # if we run out of time, skip the rest of the tests in
+        # the test plan
+        if self.expires and self.expires < time.time():
+            pytest.skip("time bound exceeded")
